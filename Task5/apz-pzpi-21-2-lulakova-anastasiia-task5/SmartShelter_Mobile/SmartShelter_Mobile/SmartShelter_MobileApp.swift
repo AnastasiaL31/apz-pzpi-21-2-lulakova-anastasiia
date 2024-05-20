eb708f02238d1b7251eb4b9ee5dc6c3edf2ab1bd
Anastasiia Lulakova <anastasiia.lulakova@nure.ua>
2024-05-20 08:31:59 +0300

    initialize mobile app

---
+++ Task5/apz-pzpi-21-2-lulakova-anastasiia-task5/SmartShelter_Mobile/SmartShelter_Mobile/SmartShelter_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartShelter_MobileApp.swift
+//  SmartShelter_Mobile
+//
+//  Created by Anastasia Lulakova on 20.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SmartShelter_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
